fcc9c241569ca31bd846e24719ac07708581b2d7
BranSmit <bransmitwork@gmail.com>
2020-09-30 23:21:48 -0700

    Solved a bug, fleshing out subclasses

--- aspen.py
+++ aspen.py
@@ -1,23 +1,42 @@
 from organism import Organism
+import random as rdm
+import math
+
 
 class Aspen(Organism):
+    
+    # Tunable rate of reproduction
+    # height * reproductionFactor = amount of new trees
+    rf = 0.3
 
-    # Overriding base organisim constructor
+    # Overwriting base organisim constructor
     def __init__(self):
-        #Extending, but keeping attributes
-        super().__init__()
+        self.ageM = 0
+        self.alive = True
         # Initializing all Aspen trees with a height of 1 cm
         self.height = 1
         # Making all Aspens capable of flowering, keep in mind this is
         # Averaging out among thousands of trees
         self.fertile = True
+        
 
-    def reproduce(self, quantity):
-        super().reproduce(quantity)
+    def reproduce(self):
+        n = self.height * Aspen.rf # Base new tree quantity
+        floor = math.floor(n-(n/4)) # Minimum new tree quantity
+        ceiling = math.ceil(n+(n/4)) # Max new tree quantity
+        #print(n, floor, ceiling)
+        # Loops depending on a random int betwixed the floor and celing
+        for _ in range(rdm.randint(floor, ceiling)): 
+            Organism.population.append(self) # Appends a clone baby object in master pop list
 
 
+test = Aspen()
 
 
+for _ in range(10):
+    test.reproduce()
+    print(Organism.population.__len__())
+    Organism.population.clear()
 
 
 
